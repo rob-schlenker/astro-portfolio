8a067356068cbab7c6af1868ea6dc05afc302f10
rob-schlenker <schlenk7@gmail.com>
2025-05-20 15:12:54 -0400

    feat: Integrate Partytown for Google Tag Manager support and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
